c5deb7e85bfcf63a4fe622e868e2d3f23caab66d
Diogo Mozão Fonseca <devmozao@outloook.com.br>
2020-09-16 23:27:25 -0300

    created a basic errorboundary component

---
+++ src/utils/errorBoundary.js
@@ -0,0 +1,28 @@
+import React, {Component, memo} from 'react'
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {hasError: false}
+  }
+
+  static getDerivedStateFromError(error) {
+    if (error) {
+      return {hasError: true}
+    }
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.log('Something went wrong.', error, errorInfo)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong. Try again later.</h1>
+    }
+
+    return this.props.children
+  }
+}
+
+export default memo(ErrorBoundary)
